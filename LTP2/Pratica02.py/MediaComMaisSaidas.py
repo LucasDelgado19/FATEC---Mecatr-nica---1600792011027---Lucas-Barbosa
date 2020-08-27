a86d19415865ab07ae70af094764e280cf200c26
LucasDelgado19 <62109188+LucasDelgado19@users.noreply.github.com>
2020-08-27 10:45:04 -0300

    Create MediaComMaisSaidas.py

---
+++ LTP2/Pratica02.py/MediaComMaisSaidas.py
@@ -0,0 +1,6 @@
+numero1 = int(input("Informe um valor:"))
+numero2 = int(input("Informe outro valor:"))
+resposta = (numero1 + numero2) / 2
+print ("A média dos valores colocados é:", resposta)
+print ("A média de %i com %i é: %f" %(numero1, numero2, resposta))
+print ("A média de {} com {} é: {}".format(numero1, numero2, resposta))
